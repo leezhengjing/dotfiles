414a20cd2d9c93044406bc180ba44adba5677ec6
leezhengjing <zhengjing.lee86@gmail.com>
2026-04-24 00:11:32 +0800

    chore: update clangd lsp
    - disable inlay hints
    - add workaround to allow #include <bits/stdc++.h> without having lsp
    throw error

--- lsp/clangd.lua
+++ lsp/clangd.lua
@@ -1,12 +1,20 @@
 return {
 	cmd = {
 		"clangd",
+		-- "--query-driver=/opt/homebrew/bin/g++-15",
+		-- "--query-driver=/usr/bin/clang++,/opt/homebrew/bin/g++-15",
+		"--query-driver=/opt/homebrew/bin/g++*",
 		"--clang-tidy",
 		"--background-index",
 		"--offset-encoding=utf-8",
 		"--compile-commands-dir=.",
 		"--header-insertion=iwyu",
 	},
+	-- init_options = {
+	-- 	inlayHints = {
+	-- 		enabled = false,
+	-- 	},
+	-- },
 	root_markers = { ".clangd", "compile_commands.json", ".git", ".clang_format" },
 	filetypes = { "c", "cpp", "objc", "objcpp" },
 	capabilities = {
