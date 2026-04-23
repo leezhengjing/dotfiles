b0c0927c9cbcefbf6234f3470397f759f1621e8b
leezhengjing <zhengjing.lee86@gmail.com>
2026-04-24 00:10:00 +0800

    chore: workaround to allow #include<bits/stdc++.h>

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,3 +1,19 @@
 return {
-	{ "neovim/nvim-lspconfig" },
+	"neovim/nvim-lspconfig",
+	opts = {
+		servers = {
+			clangd = {
+				init_options = {
+					fallbackFlags = {
+						"-std=c++20",
+						"-stdlib=libstdc++",
+						"-isystem",
+						"/opt/homebrew/Cellar/gcc/15.2.0/include/c++/15.2.0",
+						"-isystem",
+						"/opt/homebrew/Cellar/gcc/15.2.0/include/c++/15.2.0/aarch64-apple-darwin",
+					},
+				},
+			},
+		},
+	},
 }
