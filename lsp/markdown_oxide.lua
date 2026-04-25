6f436445ff9a218f0463f0b6b65a5d809de7386d
leezhengjing <zhengjing.lee86@gmail.com>
2026-04-25 17:54:02 +0800

    lsp: add markdown_oxide

---
+++ lsp/markdown_oxide.lua
@@ -0,0 +1,19 @@
+return {
+	root_markers = { ".git", ".obsidian", ".moxide.toml" },
+	filetypes = { "markdown" },
+	cmd = { "markdown-oxide" },
+	on_attach = function(client, bufnr)
+		for _, cmd in ipairs({ "today", "tomorrow", "yesterday" }) do
+			vim.api.nvim_buf_create_user_command(
+				bufnr,
+				"Lsp" .. ("%s"):format(cmd:gsub("^%l", string.upper)),
+				function()
+					command_factory(client, bufnr, cmd)
+				end,
+				{
+					desc = ("Open %s daily note"):format(cmd),
+				}
+			)
+		end
+	end,
+}
